6cb4b768458f2bb74103ac57079d50b9c6e61327
CJ3021998 <CJ3021998@LAB03-PC12>
2024-10-03 14:15:24 -0300

    ppppppppppppppppppppppppppp

---
+++ Golden Ed shop/View/Loginhaha.cs
@@ -0,0 +1,44 @@
+﻿using Golden_Ed_shop.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Golden_Ed_shop
+{
+    public partial class Loginhaha : Form
+    {
+        public Loginhaha()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            Menuprince6 frm = new Menuprince6();
+            this.Visible = false;
+            frm.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void Loginhaha_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
